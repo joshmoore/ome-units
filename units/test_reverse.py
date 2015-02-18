14a52565eb53c4d83732437d17565748cffa92f1
Sebastien Besson <seb.besson@gmail.com>
2015-02-18 13:12:42 +0000

    Create separate lists for conversion sources

--- units/test_reverse.py
+++ units/test_reverse.py
@@ -2,15 +2,23 @@ import pytest
 from conversions import Conversions
 
 
-def find_all_conversions():
-    """List all conversion keys as (category, from_unit, to_unit) tuples"""
+def list_converted_units():
+    """List all conversion keys as (category, from_unit) tuples"""
     keys = []
     categories = Conversions.keys()
     for category in categories:
         conversion_dict = Conversions[category]
         for to_unit in conversion_dict.keys():
-            for from_unit in conversion_dict[to_unit].keys():
-                keys.append((category, from_unit, to_unit))
+            keys.append((category, to_unit))
+    return keys
+
+
+def list_conversions():
+    """List all conversions as (category, from_unit, to_unit) tuples"""
+    keys = []
+    for category, to_unit in list_converted_units():
+        for from_unit in Conversions[category][to_unit].keys():
+            keys.append((category, to_unit, from_unit))
     return keys
 
 
@@ -30,7 +38,7 @@ def get_reverse_transform(transform):
         (transform[1][0], -transform[1][1]))
 
 
-conversion_keys = find_all_conversions()
+conversion_keys = list_conversions()
 conversion_ids = ["-".join(x) for x in conversion_keys]
 
 
