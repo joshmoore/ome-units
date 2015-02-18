fbffac90ce65dc0326e37f060d269ae258f4dea7
Sebastien Besson <seb.besson@gmail.com>
2015-02-18 12:55:33 +0000

    Convert into py.test

--- units/test_reverse.py
+++ units/test_reverse.py
@@ -1,3 +1,4 @@
+import pytest
 from conversions import Conversions
 
 
@@ -11,6 +12,12 @@ def find_all_conversions():
                 keys.append((category, from_unit, to_unit))
     return keys
 
-for key in find_all_conversions():
-    category, from_unit, to_unit = key
-    assert from_unit in Conversions[category][to_unit]
+conversion_keys = find_all_conversions()
+
+
+class TestReverse(object):
+
+    @pytest.mark.parametrize('key', conversion_keys)
+    def test_reverse_transform_exists(self, key):
+        category, from_unit, to_unit = key
+        assert from_unit in Conversions[category][to_unit]
